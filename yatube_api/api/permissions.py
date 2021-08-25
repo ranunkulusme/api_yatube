0f52f059c4060ab93c3a97bdb40a71099892189f
ranunkulus <lyutikov@sasagro.ru>
2021-08-25 11:30:07 +0300

    тело метода приведено к одной строке

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,7 +8,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user):
             return True
-
-        return obj.author == request.user
