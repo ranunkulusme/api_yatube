51c36da6255f465e9d3b14d5f8f033e4c0c5b239
ranunkulus <lyutikov@sasagro.ru>
2021-08-25 14:12:00 +0300

    edit endpoints

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken import views
 
 from .views import (CommentDetailView, CommentsListView, GroupView,
                     PostDetailView, PostList)
 
 urlpatterns = [
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('api/v1/posts/', PostList.as_view()),
-    path('api/v1/posts/<int:post_id>/', PostDetailView.as_view()),
-    path('api/v1/posts/<int:post_id>/comments/', CommentsListView.as_view()),
-    path('api/v1/posts/<int:post_id>/comments/<int:comment_id>/',
-         CommentDetailView.as_view()),
-    path('api/v1/groups/', GroupView.as_view({'get': 'list'})),
-    path('api/v1/groups/<int:group_id>/', GroupView.as_view(
-        {'get': 'retrieve'})),
+    path('v1/', include([
+        path('api-token-auth/', views.obtain_auth_token),
+        path('posts/', PostList.as_view()),
+        path('posts/<int:post_id>/', PostDetailView.as_view()),
+        path('posts/<int:post_id>/comments/', CommentsListView.as_view()),
+        path('posts/<int:post_id>/comments/<int:comment_id>/',
+             CommentDetailView.as_view()),
+        path('groups/', GroupView.as_view({'get': 'list'})),
+        path('groups/<int:group_id>/', GroupView.as_view({'get': 'retrieve'})),
+    ]))
 ]
