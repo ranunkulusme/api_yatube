27ce52e932ef07f5b5e0729b657760d388304c19
ranunkulus <lyutikov@sasagro.ru>
2021-08-26 16:32:11 +0300

    убраны не нужные докстринги

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,6 +8,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        if (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user):
-            return True
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
