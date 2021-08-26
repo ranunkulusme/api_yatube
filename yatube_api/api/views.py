93ec8d6a133a1c57297047ab5da1b478c9eee44b
ranunkulus <lyutikov@sasagro.ru>
2021-08-26 16:35:33 +0300

    удалены лишние вьюсеты

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,27 +6,18 @@ from posts.models import Comment, Group, Post
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
-class PostListView(ModelViewSet):
+class PostView(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    lookup_url_kwarg = 'post_id'
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class PostDetailView(ModelViewSet):
-    serializer_class = PostSerializer
-    lookup_url_kwarg = 'post_id'
-
-    def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        new_queryset = Post.objects.filter(pk=post_id)
-        return new_queryset
-
-
 class CommentsListView(ModelViewSet):
     serializer_class = CommentSerializer
-    lookup_url_kwarg = 'post_id'
+    lookup_url_kwarg = 'comment_id'
 
     def get_queryset(self):
         post_id = self.kwargs['post_id']
@@ -36,30 +27,6 @@ class CommentsListView(ModelViewSet):
     def perform_create(self, serializer):
         post_id = self.kwargs['post_id']
         serializer.save(author=self.request.user, post_id=post_id)
-
-
-class CommentDetailView(ModelViewSet):
-    serializer_class = CommentSerializer
-    lookup_url_kwarg = 'comment_id'
-
-    def get_queryset(self):
-        post_id = self.kwargs['post_id']
-        comments = Comment.objects.filter(post=post_id)
-        return comments
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied(
-                'Изменение чужого контента запрещено!'
-            )
-        super(CommentDetailView, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied(
-                'Изменение чужого контента запрещено!'
-            )
-        super(CommentDetailView, self).perform_destroy(instance)
 
 
 class GroupView(ReadOnlyModelViewSet):
