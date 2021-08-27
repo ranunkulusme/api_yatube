6e53403512c07a13c06b4c66124c69145a04096a
ranunkulus <lyutikov@sasagro.ru>
2021-08-27 11:18:41 +0300

    эндпоинты api вынесены в urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,21 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views as rest_views
 
-from api import views
-
-router = DefaultRouter()
-router.register(r'posts', views.PostView)
-router.register(r'^posts/(?P<post_id>\d+)/comments', views.CommentsView,
-                basename='comments')
-router.register(r'groups', views.GroupView)
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
-    path('api/v1/api-token-auth/', rest_views.obtain_auth_token),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
 
